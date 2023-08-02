344813abf8648ae794095cd449fa684fd3f220b1
BIAGIO AZZARELLI <b.azzarelli@gmail.com>
2023-08-01 22:08:09 -0700

    Prettier formatter and husky precommit hook runner

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,6 @@
 import { comfortaa } from "@/app/utils/fonts";
 import Link from 'next/link';
+import Image from "next/image";
 
 export default function Header() {
     return (
@@ -13,7 +14,7 @@ export default function Header() {
                 <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
-                            <img src="/images/sample-avatar.jpg" />
+                            <Image alt="user avatar" src="/images/sample-avatar.jpg" />
                         </div>
                     </label>
                     <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 z-10">
