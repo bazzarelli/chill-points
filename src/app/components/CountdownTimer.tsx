8440f948c981a244f59440af41fdcf2a2f67f5e0
BIAGIO AZZARELLI <b.azzarelli@gmail.com>
2023-08-03 15:19:04 -0700

    Added Zustand persist and devtools, testing pre-commit.

--- src/app/components/CountdownTimer.tsx
+++ src/app/components/CountdownTimer.tsx
@@ -2,50 +2,57 @@ import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import { useBreathSessionStore } from "@/app/hooks/useBreathSessionStore";
 
 type RenderTimeProps = {
-    remainingTime: number;
+  remainingTime: number;
+};
+
+const ClockText = function ({ children }: { children: string }) {
+  return <div className="text-4xl text-sky-300/90">{children}</div>;
 };
 
 const renderTime = ({ remainingTime }: RenderTimeProps) => {
-    if (remainingTime === 0) {
-        return <div className="text-3xl text-sky-300/90">Done</div>;
-    }
+  if (remainingTime === 0) {
+    return <ClockText>Done</ClockText>;
+  }
 
-    const minutes = Math.floor(remainingTime / 60)
-    const seconds = remainingTime % 60
-    const secondsFormatted = seconds < 10 ? `0${seconds}` : seconds
-    const timeFormatted = minutes ? `${minutes}:${secondsFormatted}` : `${secondsFormatted}`
+  const minutes = Math.floor(remainingTime / 60);
+  const seconds = remainingTime % 60;
+  const secondsFormatted = seconds < 10 ? `0${seconds}` : seconds;
+  const timeFormatted = minutes
+    ? `${minutes}:${secondsFormatted}`
+    : `${secondsFormatted}`;
 
-    return (
-        <div className="text-4xl text-sky-300/90">
-            {timeFormatted}
-        </div>
-    );
+  return <ClockText>{timeFormatted}</ClockText>;
 };
 
 type CountdownTimerProps = {
-    duration: number;
-    isPlaying: boolean;
-}
+  duration: number;
+  isPlaying: boolean;
+};
 
-export default function CountdownTimer({ duration, isPlaying }: CountdownTimerProps) {
-    const timerComplete = useBreathSessionStore(state => state.toggleSessionStatus);
-    
-    return (
-        <CountdownCircleTimer
-            isPlaying={isPlaying}
-            duration={duration}
-            updateInterval={0}
-            strokeWidth={4}
-            trailStrokeWidth={10}
-            size={160}
-            trailColor={"#1b2533"}
-            colors={"#10b981"}
-            onComplete={() => {
-                timerComplete(); // set state
-                ({ shouldRepeat: false })
-            }}
-        >
-            {renderTime}
-        </CountdownCircleTimer>
-    );
+export default function CountdownTimer({
+  duration,
+  isPlaying,
+}: CountdownTimerProps) {
+  const timerComplete = useBreathSessionStore(
+    (state) => state.setSessionStatus,
+  );
+
+  return (
+    <CountdownCircleTimer
+      isPlaying={isPlaying}
+      duration={duration}
+      updateInterval={0}
+      strokeWidth={4}
+      trailStrokeWidth={10}
+      size={160}
+      trailColor={"#1b2533"}
+      colors={"#10b981"}
+      onComplete={() => {
+        timerComplete(true); // set state
+        ({ shouldRepeat: false });
+      }}
+    >
+      {renderTime}
+    </CountdownCircleTimer>
+  );
 }
--- src/app/hooks/useBreathSessionStore.ts
+++ src/app/hooks/useBreathSessionStore.ts
@@ -1,21 +1,56 @@
 import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
-export type Sessions = {
-    cycleCount: number;
-    incrementCycleCount: () => void;
-    inhaleTimes: number[];
-    setInhaleTimes: (inhaleTimes: number) => void;   
-    isComplete: boolean;
-    toggleSessionStatus: () => void;
-    sessions: number[][];
-}
+type State = {
+  cycleCount: number;
+  inhaleTimes: number[];
+  isComplete: boolean;
+  sessionsData: { date: string; inhaleTimes: number[] }[];
+};
 
-export const useBreathSessionStore = create<Sessions>()((set) => ({
-    cycleCount: 0,
-    incrementCycleCount: () => set((state) => ({ cycleCount: state.cycleCount + 1 })),
-    inhaleTimes: [],
-    setInhaleTimes: (inhaleTimes) => set((state) => ({ inhaleTimes: [...state.inhaleTimes, inhaleTimes] })),
-    isComplete: false,
-    toggleSessionStatus: () => set((state) => ({ isComplete: !state.isComplete })),
-    sessions: [],
-}));
+type Actions = {
+  incrementCycleCount: () => void;
+  setInhaleTimes: (inhaleTimes: number) => void;
+  setSessionsData: () => void;
+  setSessionStatus: (isComplete: boolean) => void;
+  reset: () => void;
+};
+
+const initialState: State = {
+  cycleCount: 0,
+  inhaleTimes: [],
+  isComplete: false,
+  sessionsData: [],
+};
+
+export const useBreathSessionStore = create<State & Actions>()(
+  devtools(
+    persist(
+      (set, get) => ({
+        ...initialState,
+        incrementCycleCount: () =>
+          set((state) => ({ cycleCount: state.cycleCount + 1 })),
+        setInhaleTimes: (inhaleTimes) =>
+          set((state) => ({
+            inhaleTimes: [...state.inhaleTimes, inhaleTimes],
+          })),
+        setSessionsData: () =>
+          set((state) => ({
+            sessionsData: [
+              ...state.sessionsData,
+              {
+                date: new Date().toLocaleDateString(),
+                inhaleTimes: get().inhaleTimes,
+                cycleCount: get().cycleCount,
+              },
+            ],
+          })),
+        setSessionStatus: (isComplete) =>
+          set(() => ({ isComplete: isComplete })),
+        reset: () => set(initialState),
+      }),
+      { name: "breath-session-storage" },
+    ),
+    { enabled: true, name: "breath-session-storage" },
+  ),
+);
