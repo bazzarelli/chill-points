e891fbc42946b7834479cacba08749ac43bae84b
BIAGIO AZZARELLI <b.azzarelli@gmail.com>
2023-08-08 16:28:39 -0700

    Gameplay states are in slightly better shape now.

--- src/app/(exercise)/history/page.tsx
+++ src/app/(exercise)/history/page.tsx
@@ -1,9 +1,14 @@
 import HistoryList from "@/app/components/HistoryList";
 import { inter } from "@/app/utils/fonts";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <section className={`${inter.className} mt-12`}>
+    <section className={`${inter.className} h-screen bg-slate-300`}>
+      <Link href="/game">
+        <button className="my-5 ml-4 btn btn-sm btn-outline">back</button>
+      </Link>
+
       <HistoryList />
     </section>
   );
