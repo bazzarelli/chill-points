15b1276ffc6f71472e89890cf0882f3849bcb927
BIAGIO AZZARELLI <b.azzarelli@gmail.com>
2023-08-13 21:44:53 -0700

    Settings for game length and bug fixes.

--- src/app/components/game/GameInstructions.tsx
+++ src/app/components/game/GameInstructions.tsx
@@ -1,20 +1,21 @@
 function GameInstructions() {
   return (
-    <div className="collapse collapse-arrow border border-sky-800 text-left text-sky-300/80 mb-6">
-      <input type="checkbox" />
-      <div className="collapse-title text-xl">Game Instructions</div>
-      <div className="collapse-content">
+    <div className="border border-sky-800 rounded-md text-left text-sky-300/80 mb-6">
+      <div className="text-xl p-4 pb-0">Game Instructions</div>
+      <div className="p-4">
         <p className="text-lg">
-          The 1-minute game begins when you press and hold the frog.
+          The game begins when you press and hold the frog.
         </p>
         <p className="pt-3">
-          Inhale through your nose for 5 seconds - the blue box grows to match
-          the frog&apos;s height. Exhale through your nose for 5 seconds - the
-          box shrinks back down.
+          Inhale through your nose - the blue box grows to match the frog&apos;s
+          height.
         </p>
         <p className="pt-3">
-          Each inhale/exhale cycle is 5 seconds and represented by a pearl. Keep
-          your finger on the frog during inhales and off during exhales.
+          Exhale through your nose - the box shrinks back down.
+        </p>
+        <p className="pt-3">
+          Each inhale/exhale cycle is represented by a pearl. Keep your finger
+          on the frog during inhales and off during exhales.
         </p>
       </div>
     </div>
