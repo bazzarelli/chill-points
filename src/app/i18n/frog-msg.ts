6c24ac3a0ba565d225ea44c9cfa5afed7725ea4b
BIAGIO AZZARELLI <b.azzarelli@gmail.com>
2023-08-18 16:30:14 -0700

    Proof of concept completion sound + per page title

--- src/app/i18n/frog-msg.ts
+++ src/app/i18n/frog-msg.ts
@@ -1,8 +1,8 @@
 export const msg = {
   intro_hook: "Unwind with a quick and calming breathing game.",
   welcome: "Press & hold frog to begin",
-  inhale: "Inhale - press frog",
-  exhale: "Exhale - release",
+  inhale: "inhale + press",
+  exhale: "exhale + release",
   finished: "Frog is impressed!",
   cancelled: "Long press during inhale",
   restart: "Start over?",
