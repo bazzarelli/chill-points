3e7977704e53a01ab76aef97f75348d02634d9a5
BIAGIO AZZARELLI <b.azzarelli@gmail.com>
2023-11-20 12:08:19 -0800

    New bookmark icon and testing open graph sharing

--- src/app/(exercise)/badges/layout.tsx
+++ src/app/(exercise)/badges/layout.tsx
@@ -2,7 +2,27 @@ export const metadata = {
   title: "Chill Points: badges",
   description: "Play the game and learn breath control.",
   appleMobileWebAppCapable: "yes",
-  appleMobileWebAppStatusBarStyle: "black",
+  openGraph: {
+    title: "Chill Points",
+    description: "Play the game and learn breath control.",
+    url: "https://chillpoints.app/game",
+    siteName: "ChillPoints.app",
+    images: [
+      {
+        url: "https://chillpoints.app/og.png",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://chillpoints.app/og.png",
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
